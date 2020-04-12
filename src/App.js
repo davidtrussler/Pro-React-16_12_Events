643d4c6af0d7cd8121a2bfaed334ec537737b758
David Trussler <mail@davidtrussler.net>
2020-04-12 18:17:28 +0100

    Updates App to add click event handlers to all HTML hierarchy

--- src/App.js
+++ src/App.js
@@ -18,14 +18,27 @@ export default class App extends Component {
     }); 
   }
 
+  handleClick = (e) => {
+    console.log(
+      `Target: ${e.target.tagName} ` + 
+      `CurrentTarget: ${e.currentTarget.tagName}`
+    ); 
+  }
+
   render() {
     return (
-      <div className="m-2">
+      <div 
+        className="m-2"
+        onClick={this.handleClick}
+      >
         <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
           {this.state.message}
         </div>
 
-        <div className="text-center">
+        <div 
+          className="text-center"
+          onClick={this.handleClick}
+        >
           <ThemeButton theme="primary" callback={this.selectTheme}/>
           <ThemeButton theme="danger" callback={this.selectTheme}/>
         </div>
