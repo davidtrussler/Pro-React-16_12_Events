99a5da1cdf9f7a5912774261564b62867670adfd
David Trussler <mail@davidtrussler.net>
2020-04-12 08:09:46 +0100

    Understanding Events - Adds onClick event to button and invokes method to handle it - Differentiates between different Event Types - Uses persist method to avoid Event Reuse Pitfall - Uses custom argument to invoke event handler - Prevents Default Behaviour

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Based on Chapter 12 of Pro-React-16
 
+See React documentation: [SyntheticEvent](https://reactjs.org/docs/events.html)
+
 ## Set up 
 
 - Run `npm install` to install dependencies
@@ -12,3 +14,13 @@ Based on Chapter 12 of Pro-React-16
 - Sets up a new app
 - Adds bootstrap
 - Creates new App component to display basic initial view
+
+### 2. [Understanding Events](https://github.com/davidtrussler/Pro-React-16_12_Events/tree/Understanding-Events)
+
+- Adds onClick event to button
+- Invokes method to handle event
+- Differentiates between different Event Types
+- Uses persist method to avoid Event Reuse Pitfall
+  - SyntheticEvents reset properties to null after event is handled
+- Uses custom argument to invoke event handler
+- Prevents Default Behaviour
--- src/App.js
+++ src/App.js
@@ -4,19 +4,60 @@ export default class App extends Component {
   constructor(props) {
     super(props); 
     this.state = {
-      message: 'Ready'
+      message: 'Ready', 
+      counter: 0, 
+      theme: 'secondary'
+    }
+  }
+
+  handleEvent = (e, newTheme) => {
+    e.persist(); 
+
+    this.setState(
+      {
+        counter: this.state.counter + 1, 
+        theme: newTheme
+      }, 
+      () => this.setState(
+        {message: `${e.type}: ${this.state.counter}`}
+      )
+    ); 
+  }
+
+  toggleCheckBox = (e) => {
+    if (this.state.counter === 0) {
+      e.preventDefault(); 
     }
   }
 
   render() {
     return (
       <div className="m-2">
-        <div className="h4 bg-primary text-white text-center p-2">
+        <div className="form-check">
+          <label>
+            <input 
+              className="form-check-input"
+              type="checkbox"
+              onClick={this.toggleCheckBox}
+            />
+            This is a checkbox
+          </label>
+        </div>
+
+        <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
           {this.state.message}
         </div>
 
         <div className="text-center">
-          <button className="btn btn-primary">Click Me</button>          
+          <button 
+            className="btn btn-primary"
+            onClick={(e) => this.handleEvent(e, 'primary')}
+          >Normal</button>
+
+          <button 
+            className="btn btn-danger m-1"
+            onClick={(e) => this.handleEvent(e, 'danger')}
+          >Danger</button>
         </div>
       </div>
     );
